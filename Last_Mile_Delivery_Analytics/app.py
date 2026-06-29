7911be44eb5a6f61feedc9d8bc7d27672191364a
Rachit Bajpai <151463118+Rachit150501@users.noreply.github.com>
2026-06-29 22:49:09 +0530

    Update app.py

--- Last_Mile_Delivery_Analytics/app.py
+++ Last_Mile_Delivery_Analytics/app.py
@@ -56,6 +56,23 @@ else:
     st.code(str(image_path))
 
 st.divider()
+st.divider()
+
+st.header("🐍 Python Analysis")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("Screenshots/Python/01_dataset_preview.png", caption="Dataset Preview")
+    st.image("Screenshots/Python/03_dataset_shape.png", caption="Dataset Shape")
+    st.image("Screenshots/Python/05_city_orders_table.png", caption="City Orders")
+
+with col2:
+    st.image("Screenshots/Python/02_missing_values.png", caption="Missing Values")
+    st.image("Screenshots/Python/04_columns_list.png", caption="Columns")
+    st.image("Screenshots/Python/06_vendor_orders_table.png", caption="Vendor Orders")
+
+st.image("Screenshots/Python/07_delay_reasons_table.png", caption="Delay Reasons")
 
 # -------------------------------
 # About Project
